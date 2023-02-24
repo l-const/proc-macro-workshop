73691342678767b9c9558b0f16f28c87941ac7b7
l-const <const_l@outlook.com>
2023-02-24 23:03:08 +0200

    builder checkpoint 5

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
-
 use quote::quote;
-use syn::{
-    parse_macro_input,
-    token::{Enum, Struct},
-    DeriveInput, Fields,
-};
+use syn::{parse_macro_input, DeriveInput};
 
 struct ParsedFields<'parse> {
     types: Vec<&'parse syn::Type>,
@@ -24,19 +19,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
         panic!("{msg}");
     }
 
+    let parsed_fields = parsed_fields.ok().unwrap();
+    let idents = parsed_fields.idents;
+    let types = parsed_fields.types;
     // Create modified TokenStream
     let tokens = quote! {
         impl #struct_ident {
 
             pub fn builder() -> #builder_ident {
-                 #builder_ident {}
+                 #builder_ident {
+                    #(#idents  : Default::default()),*
+                 }
             }
         }
 
-        pub struct #builder_ident;
+        pub struct #builder_ident {
+            #(#idents : #types),*
+        }
 
         impl #builder_ident {
+            #(pub fn #idents(&mut self, #idents: #types) -> &mut #builder_ident {
+                self.#idents = #idents;
+                self
+            })*
 
+            pub fn build(&mut self) -> std::result::Result<#struct_ident, Box<dyn std::error::Error>> {
+                Ok(#struct_ident {
+                    #(#idents : self.#idents.clone()),*
+                })
+            }
         }
     };
 
@@ -48,17 +59,24 @@ fn create_builder_ident(struct_ident: &Ident) -> Ident {
     Ident::new(&format!("{}Builder", struct_ident), Span::call_site())
 }
 
+fn find_optional_types(parse_types: &[Ident]) -> Option<&[Ident]> {
+    unimplemented!()
+}
+
 fn parse_fields(derive_input: &DeriveInput) -> Result<ParsedFields<'_>, &'static str> {
     pub use syn::Data::*;
     if let Struct(stru) = &derive_input.data {
         pub use syn::Fields::*;
         let fields = &stru.fields;
-        if let Named(namedFields) = fields {
-            let iter_ty = namedFields.named.iter();
+        if let Named(named_fields) = fields {
+            let iter_ty = named_fields.named.iter();
             let iter_ident = iter_ty.clone();
             let vec_ty: Vec<&syn::Type> = iter_ty.map(|f| &f.ty).collect();
             let vec_ident: Vec<&proc_macro2::Ident> =
                 iter_ident.map(|f| f.ident.as_ref().unwrap()).collect();
+            if vec_ident.len() != vec_ty.len() {
+                return Err("Mismatched parsed length of identifiers,types");
+            }
             return Ok(ParsedFields {
                 types: vec_ty,
                 idents: vec_ident,
